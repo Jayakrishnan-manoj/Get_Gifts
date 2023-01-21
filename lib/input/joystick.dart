2c87156266b537fa27a243a7840595f3f0f9ace2
Jayakrishnan-manoj <91311543+Jayakrishnan-manoj@users.noreply.github.com>
2023-01-21 20:17:49 +0530

    style: change joystick theme and position

--- lib/input/joystick.dart
+++ lib/input/joystick.dart
@@ -5,14 +5,14 @@ import 'package:flutter/cupertino.dart';
 JoystickComponent joystick = JoystickComponent(
   knob: CircleComponent(
     radius: 30,
-    paint: BasicPalette.red.withAlpha(200).paint(),
+    paint: BasicPalette.blue.withAlpha(200).paint(),
   ),
   background: CircleComponent(
     radius: 80,
-    paint: BasicPalette.red.withAlpha(100).paint(),
+    paint: BasicPalette.blue.withAlpha(100).paint(),
   ),
   margin: const EdgeInsets.only(
-    left: 40,
+    right: 40,
     bottom: 40,
   ),
 );
